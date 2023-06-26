2ddb8c161f9a7b918933e27575e3c086b5c798aa
Thien Nguyen <s3426515@rmit.edu.vn>
2023-06-27 09:23:58 +1000

    [Suitelet] Add operations

--- SuiteScripts/mp_sl_mass_email_tn_v2_vue.js
+++ SuiteScripts/mp_sl_mass_email_tn_v2_vue.js
@@ -13,6 +13,71 @@ const defaultTitle = 'Mass Email Sender';
 
 let NS_MODULES = {};
 
+let EMAIL_TEMPLATE_FIELDS = [
+    "_csrf",
+    "_eml_nkey_",
+    "addcompanyaddress",
+    "addunsubscribelink",
+    "content",
+    "entryformquerystring",
+    "id",
+    "isautoconverted",
+    "isinactive",
+    "isprivate",
+    "name",
+    "nsapiCT",
+    "numchildrecords",
+    "restrictaccess",
+    "scriptid",
+    "subject",
+    "sys_id",
+    "templatetype",
+    "templateversion",
+    "type",
+    "typename",
+    "usesmedia",
+    "nsapiPI",
+    "nsapiSR",
+    "nsapiVF",
+    "nsapiFC",
+    "nsapiPS",
+    "nsapiVI",
+    "nsapiVD",
+    "nsapiPD",
+    "nsapiVL",
+    "nsapiRC",
+    "nsapiLI",
+    "nsapiLC",
+    "nsbrowserenv",
+    "wfPI",
+    "wfSR",
+    "wfVF",
+    "wfFC",
+    "wfPS",
+    "_multibtnstate_",
+    "selectedtab",
+    "externalid",
+    "whence",
+    "customwhence",
+    "wfinstances",
+    "publisherid",
+    "package",
+    "recordtype",
+    "description",
+    "helpmemo",
+    "dummy0-2",
+    "mediaitem",
+    "dummy0-10",
+    "dummy4",
+    "dummy4-1",
+    "mergetypestpl_en",
+    "mergefieldstpl_en",
+    "dummy5",
+    "dummy6",
+    "restricttogroup",
+    "campaigndomain"
+];
+
 
 define(['N/ui/serverWidget', 'N/render', 'N/search', 'N/file', 'N/log', 'N/record', 'N/email', 'N/runtime', 'N/https', 'N/task', 'N/format', 'N/url'],
     (serverWidget, render, search, file, log, record, email, runtime, https, task, format, url) => {
@@ -158,16 +223,147 @@ function _writeResponseJson(response, body) {
 }
 
 const getOperations = {
+    'getAllEmailTemplates' : function (response) {
+        let {search} = NS_MODULES;
+        let data = [];
+        let columnNames = ['internalid', 'name', 'subject'];
 
+        search.create({
+            type: search.Type['EMAIL_TEMPLATE'],
+            filters: [],
+            columns: columnNames.map(item => ({name: item}))
+        }).run().each(result => {
+            let tmp = {};
+
+            for (let column of result.columns)
+                tmp[column.name] = result.getValue(column);
+
+            data.push(tmp);
+
+            return true;
+        });
+
+        _writeResponseJson(response, data);
+    },
+    'getAllSavedSearches' : function (response) {
+        let {search} = NS_MODULES;
+
+        let data = [];
+        let columnNames = ['id', 'title', 'recordtype', 'frombundle', 'owner', 'access', 'lastrunby', 'lastrunon'];
+
+        search.create({
+            type: search.Type['SAVED_SEARCH'],
+            filters: [
+                // {name: 'recordtype', operator: 'is', values: 'Customer'},
+            ],
+            columns: columnNames.map(item => ({name: item}))
+        }).run().each(result => {
+            let tmp = {};
+
+            for (let column of result.columns)
+                tmp[column.name] = result.getValue(column);
+
+            data.push(tmp);
+
+            return true;
+        });
+
+        _writeResponseJson(response, data);
+    },
+    'getEmailAddressesFromSavedSearch' : function (response, {savedSearchId}) {
+        _writeResponseJson(response, sharedFunctions.getEmailAddressesFromSavedSearch(savedSearchId));
+    },
+    'getEmailTemplate' : function (response, {emailTemplateId}) {
+        _writeResponseJson(response, sharedFunctions.getEmailTemplate(emailTemplateId));
+    }
 }
 
 const postOperations = {
+    'sendTestEmail' : function (response, {emailTemplateId}) {
+        let {runtime, email} = NS_MODULES;
+        let {emailSubject, emailBody} = sharedFunctions.getEmailTemplate(emailTemplateId);
 
+        email.sendBulk({
+            author: 112209,
+            recipients: [runtime.getCurrentUser().email],
+            subject: emailSubject,
+            body: emailBody,
+            isInternalOnly: true
+        });
+
+        _writeResponseJson(response, {recipient: runtime.getCurrentUser().email});
+    },
+    'sendMassEmails' : function (response, {savedSearchId, emailTemplateId}) {
+        let {email} = NS_MODULES;
+        let emailAddresses = sharedFunctions.getEmailAddressesFromSavedSearch(savedSearchId);
+        let {emailSubject, emailBody} = sharedFunctions.getEmailTemplate(emailTemplateId);
+
+        if (emailAddresses.length) {
+            email.sendBulk({
+                author: 112209, // sending as system@sent-via.netsuite.com
+                body: emailBody,
+                subject: emailSubject,
+                recipients: emailAddresses,
+                isInternalOnly: false
+            });
+
+            _writeResponseJson(response, 'Emails have been sent to all addresses present in the specified saved search.');
+        } else _writeResponseJson(response, 'No email was sent. Saved search contain no email address.');
+    },
 };
 
 
 const sharedFunctions = {
+    getEmailTemplate(emailTemplateId) {
+        let {render} = NS_MODULES;
+        let mergeResult = render.mergeEmail({
+            templateId: emailTemplateId,
+            entity: null,
+            recipient: null,
+            supportCaseId: null,
+            transactionId: null,
+            customRecord: null
+        });
+        let emailSubject = mergeResult.subject;
+        let emailBody = mergeResult.body;
 
+        return {emailSubject, emailBody};
+    },
+    getEmailAddressesFromSavedSearch(savedSearchId) {
+        let {search} = NS_MODULES;
+        let data = [];
+        let fieldsToGet = [];
+        let fieldsToCheck = ['email', 'custentity_email_service', 'custentity_email_sales'];
+        let fieldsAreChecked = false;
+        let chunk = 0;
+        let resultSubset = [];
+
+        let resultSet = search.load({id: savedSearchId}).run();
+
+        do {
+            resultSubset = resultSet.getRange({start: chunk * 1000, end: chunk * 1000 + 1000});
+
+            for (let result of resultSubset) {
+                if (!fieldsAreChecked) {
+                    for (let column of result.columns)
+                        if (fieldsToCheck.includes(column.name)) fieldsToGet.push(column.name);
+
+                    fieldsAreChecked = true;
+                }
+
+                if (!fieldsToGet.length) break;
+
+                for (let field of fieldsToGet)
+                    if (result.getValue(field)) data.push(result.getValue(field));
+
+                chunk++;
+            }
+
+            if (!fieldsToGet.length) break;
+        } while(resultSubset.length >= 1000 && fieldsToGet.length)
+
+        return [...new Set(data)]; // we use 'new Set()' to eliminate duplicates
+    }
 };
 
 function _parseIsoDatetime(dateString) {
