fd46dfe5b7d804c5642c414d54da723bb1d15652
Thien Nguyen <s3426515@rmit.edu.vn>
2023-07-04 01:10:20 +1000

    Change how Email Template preview works

--- src/views/mass-email/Main.vue
+++ src/views/mass-email/Main.vue
@@ -1,34 +1,33 @@
 <template>
     <div class="row">
-        <div class="col-9 mb-4">
-            <b-input-group prepend="Saved Search">
+        <div class="col-4 mb-4">
+            <b-input-group prepend="Search Type">
+                <b-form-select v-model="form.searchType" :options="$store.getters['searchTypes']" @change="$store.dispatch('handleSearchTypeChanged')"></b-form-select>
+            </b-input-group>
+        </div>
+        <div class="col-8 mb-4">
+            <b-input-group :prepend="form.searchType || 'No Search Type'">
                 <SearchableSelect v-model="form.savedSearchId" :options="savedSearches" :max-height="'30vh'"
-                                  v-validate="'required'" data-vv-name="saved_search" :disabled="busy"
+                                  v-validate="'required'" data-vv-name="saved_search" :disabled="form.busy || !form.searchType"
                                   :class="errors.has('saved_search') ? 'is-invalid' : ''"/>
 
                 <b-form-invalid-feedback :state="!errors.has('saved_search')">{{ errors.first('saved_search') }}</b-form-invalid-feedback>
             </b-input-group>
         </div>
 
-        <div class="col-3 mb-4">
-            <b-button class="w-100" @click="$store.dispatch('previewEmailAddresses')">Retrieve Email Addresses</b-button>
-        </div>
-
-        <div class="col-9 mb-4">
+        <div class="col-12 mb-2">
             <b-input-group prepend="Email Template">
                 <SearchableSelect v-model="form.emailTemplateId" :options="emailTemplates" :max-height="'30vh'"
                                   v-validate="'required'" data-vv-name="saved_search" :disabled="busy"
-                                  :class="errors.has('email_template') ? 'is-invalid' : ''"/>
+                                  :class="errors.has('email_template') ? 'is-invalid' : ''" @input="$store.dispatch('previewEmailTemplate')"/>
 
                 <b-form-invalid-feedback :state="!errors.has('email_template')">{{ errors.first('email_template') }}</b-form-invalid-feedback>
             </b-input-group>
         </div>
 
-        <div class="col-3 mb-4">
-            <b-button class="w-100" @click="$store.dispatch('previewEmailTemplate')">Preview Email Template</b-button>
-        </div>
+        <PreviewEmailTemplate />
 
-        <div class="col-12 mb-4">
+        <div class="col-12 mb-4" v-show="!!$store.getters['previewTemplateModal'].emailBody">
             <b-button class="mx-3" @click="$store.dispatch('sendTestEmail')">Send Myself Test Email</b-button>
 
             <SendMassEmailButton />
@@ -46,22 +45,36 @@ import SearchableSelect from "@/components/SearchableSelect";
 import PreviewEmailTemplateModal from "@/views/mass-email/PreviewEmailTemplateModal";
 import PreviewEmailAddressModal from "@/views/mass-email/PreviewEmailAddressModal";
 import SendMassEmailButton from "@/views/mass-email/SendMassEmailButton";
+import PreviewEmailTemplate from "@/views/mass-email/PreviewEmailTemplate";
+import http from "@/utils/http";
 
 export default {
     name: "Main",
-    components: {SendMassEmailButton, PreviewEmailAddressModal, PreviewEmailTemplateModal, SearchableSelect},
+    components: {
+        PreviewEmailTemplate,
+        SendMassEmailButton, PreviewEmailAddressModal, PreviewEmailTemplateModal, SearchableSelect},
+    data: () => ({
+        type: '',
+        types: [
+            {value: 'customer', text: 'Customer'},
+            {value: 'contact', text: 'Customer'},
+            {value: 'customer', text: 'Customer'},
+        ]
+    }),
+    methods: {
+        testMethod() {
+            http.post('testMethod').then(msg => console.log(msg));
+        },
+    },
     computed: {
         emailTemplates() {
             return this.$store.getters['emailTemplates'].map(item => ({
-                value: item.internalid,
+                value: item.custrecord_camp_comm_email_template,
                 text: item.name
             }));
         },
         savedSearches() {
-            return this.$store.getters['savedSearches'].map(item => ({
-                value: item.id,
-                text: item.title
-            }));
+            return this.$store.getters['savedSearches'];
         },
         form() {
             return this.$store.getters['form']
