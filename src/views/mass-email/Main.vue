ff25eee3d6b6b31b72d40f8aa4ae1462ea86090d
Thien Nguyen <s3426515@rmit.edu.vn>
2023-07-04 10:22:22 +1000

    Remove unused code

--- src/views/mass-email/Main.vue
+++ src/views/mass-email/Main.vue
@@ -2,26 +2,24 @@
     <div class="row">
         <div class="col-4 mb-4">
             <b-input-group prepend="Search Type">
-                <b-form-select v-model="form.searchType" :options="$store.getters['searchTypes']" @change="$store.dispatch('handleSearchTypeChanged')"></b-form-select>
+                <b-form-select v-model="form.searchType" :options="$store.getters['searchTypes']"
+                               @change="$store.dispatch('handleSearchTypeChanged')"
+                               :disabled="form.busy || !form.searchType"></b-form-select>
             </b-input-group>
         </div>
         <div class="col-8 mb-4">
             <b-input-group :prepend="form.searchType || 'No Search Type'">
                 <SearchableSelect v-model="form.savedSearchId" :options="savedSearches" :max-height="'30vh'"
-                                  v-validate="'required'" data-vv-name="saved_search" :disabled="form.busy || !form.searchType"
+                                  :disabled="form.busy || !form.searchType"
                                   :class="errors.has('saved_search') ? 'is-invalid' : ''"/>
-
-                <b-form-invalid-feedback :state="!errors.has('saved_search')">{{ errors.first('saved_search') }}</b-form-invalid-feedback>
             </b-input-group>
         </div>
 
         <div class="col-12 mb-2">
             <b-input-group prepend="Email Template">
                 <SearchableSelect v-model="form.emailTemplateId" :options="emailTemplates" :max-height="'30vh'"
-                                  v-validate="'required'" data-vv-name="saved_search" :disabled="busy"
-                                  :class="errors.has('email_template') ? 'is-invalid' : ''" @input="$store.dispatch('previewEmailTemplate')"/>
-
-                <b-form-invalid-feedback :state="!errors.has('email_template')">{{ errors.first('email_template') }}</b-form-invalid-feedback>
+                                  :disabled="busy" :class="errors.has('email_template') ? 'is-invalid' : ''"
+                                  @input="$store.dispatch('previewEmailTemplate')"/>
             </b-input-group>
         </div>
 
@@ -53,18 +51,8 @@ export default {
     components: {
         PreviewEmailTemplate,
         SendMassEmailButton, PreviewEmailAddressModal, PreviewEmailTemplateModal, SearchableSelect},
-    data: () => ({
-        type: '',
-        types: [
-            {value: 'customer', text: 'Customer'},
-            {value: 'contact', text: 'Customer'},
-            {value: 'customer', text: 'Customer'},
-        ]
-    }),
     methods: {
-        testMethod() {
-            http.post('testMethod').then(msg => console.log(msg));
-        },
+
     },
     computed: {
         emailTemplates() {
